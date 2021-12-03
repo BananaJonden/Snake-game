123e8c0eb42e4e1b9ecd9ddaa3384c340da314bf
BananaJonden <91635129+BananaJonden@users.noreply.github.com>
2021-12-02 22:46:17 -0800

    Update snake code.py

--- snake code.py
+++ snake code.py
@@ -1,1 +1,42 @@
-print('hi')
+import turtle as trtl
+
+wn = trtl.Screen()
+turtle = trtl.Turtle()
+food_balls = trtl.Turtle()
+pacman = "pacman.gif"
+turtle.turtlesize(1)
+wn.addshape(pacman)
+turtle.shape(pacman)
+turtle.penup()
+
+food_balls.fillcolor("red")
+food_balls.begin_fill()
+food_balls.circle(15)
+food_balls.end_fill()
+def move_right():
+    turtle.setheading(0)
+    turtle.forward(25)
+
+
+def move_up():
+    turtle.setheading(90)
+    turtle.forward(25)
+
+
+def move_left():
+    turtle.setheading(180)
+    turtle.forward(25)
+
+
+def move_down():
+    turtle.setheading(270)
+    turtle.forward(25)
+
+
+
+wn.onkeypress(move_right, "d")
+wn.onkeypress(move_up, "w")
+wn.onkeypress(move_left, "a")
+wn.onkeypress(move_down, "s")
+wn.listen()
+wn.exitonclick()
